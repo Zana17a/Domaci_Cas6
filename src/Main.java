c0cd6424278ead96cd0053d3aff76c8c66412c98
ZanaArezina <zana.arezina.p3@gmail.com>
2024-04-11 14:11:10 +0200

    "update"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.*;
+
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Domaci Cas6:");
+
+//        ArrayList <String> listaPolozili = new ArrayList<>();
+//        ArrayList <String> listaPali = new ArrayList<>();
+            List listaPolozili = new ArrayList();
+            List listaPali = new ArrayList();
+
+        while (true) {
+            Student student = new Student();
+                 System.out.println("Unos ime: ");
+                 student.ime = scanner.next();
+            if (student.ime.equalsIgnoreCase("quit")) {
+                break;}
+                 System.out.println("Unos prezime: ");
+                 student.prezime = scanner.next();
+            if (student.prezime.equalsIgnoreCase("quit")) {
+                break;
+            }
+                System.out.println("Unos ocenu: ");
+                student.ocena = scanner.nextInt();
+            if (student.ocena > 5) listaPolozili.add(student.ime +" "+ student.prezime);
+            else listaPali.add(student.ime +" "+ student.prezime);
+        }
+        System.out.println("Polozili: " + listaPolozili);
+        System.out.println("Pali: " + listaPali);
+
+
+
+
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    String ime;
+    String prezime;
+    int ocena;
+
+
+//    public Student(String ime, String prezime) {
+//        this.ime = ime;
+//        this.prezime = prezime;}
+
+    public Student() {
+
+    }
+//    public String ispis2(){
+//        return "Student{" + "ime='" + this.ime + '\'' +  //sad je greyed out lokalna varijavla ime, zato sto ovde stoji THIS.IME sto znaci uzmi globalnu varijablu
+//                    ", prezime='" + prezime + '\'' +
+//                    ", godine=" + ocena +
+//                    '}';
+//        }
+
+//    public void ispisPolozili (List listaPolozili){            //zbog void mogu da isprintam reziltat ali ne mogu da ga smestim u varijablu
+//        System.out.println("Polozili: " + listaPolozili);
+//    }
+
+    }
+
